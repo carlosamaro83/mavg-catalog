886f9ec4655ccfa1f3ca7e9e725bca11d4e4a9e1
carlosamaro83 <carlosamarosoto@gmail.com>
2025-11-09 04:33:12 +0100

    Remove unnecessary comment from vite.config.js
    Removed comment about changing repository name.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// ⚙️ Importante: cambia "mavg-catalog" por el nombre exacto de tu repositorio
 export default defineConfig({
   base: '/mavg-catalog/',
   plugins: [react()],
