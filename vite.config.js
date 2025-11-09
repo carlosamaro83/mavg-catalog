559ec4b727569784aa43b9798a1eecc421ced165
carlosamaro83 <carlosamarosoto@gmail.com>
2025-11-09 04:24:43 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// ⚙️ Importante: cambia "mavg-catalog" por el nombre exacto de tu repositorio
 export default defineConfig({
+  base: '/mavg-catalog/',
   plugins: [react()],
 })
